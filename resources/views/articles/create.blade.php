04fdcc170c0ce3a207c33598c6b31b150ad93488
YukiYouda <youda0518@yahoo.co.jp>
2021-10-18 22:30:25 +0900

    登録画面実装

---
+++ resources/views/articles/create.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+@section('titel', '新規登録')
+@section('content')
+    <div class="col-8 col-offset-2 mx-auto">
+        <h1>新規登録</h1>
+        <form action="{{ route('articles.store') }}" method="post" enctype="multipart/form-data">
+            <div class="card mb-3">
+                @csrf
+                <div class="row m-3">
+                    <div class="mb-3">
+                        <label for="file">ファイルを選択してください</label>
+                        <input type="file" name="file" id="file">
+                    </div>
+                    <div class="mb-3">
+                        <label for="caption">イメージの説明を入力してください</label>
+                        <input type="text" name="caption" id="caption">
+                    </div>
+                    <div>
+                        <label for="info">イメージの説明を入力してください</label>
+                        <textarea name="info" id="info" rows="5"></textarea>
+                    </div>
+                </div>
+            </div>
+            <input type="submit">
+        </form>
+    </div>
+@endsection
